dd9c7853a8473da77ddc7263148da57b5b8fd365
DANNY SIMWINGA <user@USERs-MacBook-Pro.local>
2025-02-23 11:43:36 +0200

    Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,22 +2,31 @@ import django
 import os
 
 # Setup Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-models.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query 1: Get all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
+    try:
+        author = Author.objects.get(name=author_name)
+        return Book.objects.filter(author=author)  # <-- Fixed this line
+    except Author.DoesNotExist:
+        return f"No books found for author: {author_name}"
 
 # Query 2: List all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return f"No books found in library: {library_name}"
 
 # Query 3: Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except Library.DoesNotExist:
+        return f"No librarian found for library: {library_name}"
